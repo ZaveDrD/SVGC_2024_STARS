1b0455d546a5c7fadfdab355d3ad7a80939e06f3
= <=>
2024-04-08 09:37:12 +1000

    Added a StaticGesture class - fyi i just copied and pasted the detect_vertebraeC6 function for detect so it might not work properly

--- BASE_GAME_FILES/main.py
+++ BASE_GAME_FILES/main.py
@@ -28,7 +28,82 @@ class Gesture:
     pass
 
 class StaticGesture(Gesture):
-    ...
+    def __init__(self, *params: list[dict[str, list[int | None]]]):
+        self.params = params
+
+    def detect(self, hands: list[list[int]]) -> bool:
+        """
+        A method to detect if the gesture is being done
+        ## Args:
+            hands: (list[list[int]]) The hand
+        ## Returns:
+            bool - True if the gesture is being done, else false
+        """
+        handsDoingGesture = []
+        None_Banned_Hands = []
+
+        for param in self.params:  # If ONE parameter is wrong, it's not true for that hand
+            if param[0][0] == param[1][0] and param[0][0] is not None:  # If its for only one hand
+                if param[0][0] >= len(hands): return []
+                if len(hands[param[0][0]]) < 21: return []
+
+                selected_hand = hands[param[0][0]]
+                distBtwPoints = abs(math.dist(selected_hand[param[0][1]], selected_hand[param[1][1]]))
+
+                if param[3]:
+                    result = distBtwPoints < param[2]
+                else:
+                    result = distBtwPoints > param[2]
+
+                if not result: return[]
+
+            if not param[0][0] == param[1][0] and param[0][0] is not None and param[1][0] is not None:
+                if param[0][0] >= len(hands) or param[1][0] >= len(hands): return []
+                if len(hands[param[0][0]]) < 21 or len(hands[param[1][0]]) < 21: return []
+
+                distBtwPoints = abs(math.dist(hands[param[0][0]][param[0][1]], hands[param[1][0]][param[1][1]]))
+
+                if param[3]:
+                    result = distBtwPoints < param[2]
+                else:
+                    result = distBtwPoints > param[2]
+
+                if not result: return[]
+
+            if param[0][0] is None or param[1][0] is None:
+                num_results_true = 0
+                for hand_num, hand in enumerate(hands):
+                    if len(hand) < 21: return[]
+
+                    distBtwPoints = abs(math.dist(hand[param[0][1]], hand[param[1][1]]))
+
+                    if param[3]:
+                        result = distBtwPoints < param[2]
+                    else:
+                        result = distBtwPoints > param[2]
+
+                    if result:
+                        if hand_num not in handsDoingGesture and hand_num not in None_Banned_Hands:
+                            handsDoingGesture.append(hand_num)
+                        num_results_true += 1
+                    else:
+                        if hand_num in handsDoingGesture:
+                            handsDoingGesture.remove(hand_num)
+                        None_Banned_Hands.append(hand_num)
+
+                if num_results_true == 0: return[]
+
+        if len(handsDoingGesture) > 0:
+            return handsDoingGesture
+        else:
+            for param in self.params:
+                if not param[0][0] in handsDoingGesture:
+                    handsDoingGesture.append(param[0][0])
+
+                if not param[1][0] in handsDoingGesture:
+                    handsDoingGesture.append(param[1][0])
+
+            return handsDoingGesture
 
 class MotionGesture(Gesture):
     def __init__(self, gesture: StaticGesture, index: list[int], *others: list[dict['gesture': StaticGesture, 'offset': list[int], 'index': list[int]]]):
