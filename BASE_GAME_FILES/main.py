fb3657601d1b94e73075ab075e4cc09808b89e2f
Zave D <Zave_D11@hotmail.com>
2024-04-08 10:09:45 +1000

    nvm it workd

--- BASE_GAME_FILES/main.py
+++ BASE_GAME_FILES/main.py
@@ -24,11 +24,14 @@ true = True
 false = False
 null = None
 
+
 class Gesture:
     pass
 
+
 class StaticGesture(Gesture):
     ...
+
 
 class MotionGesture(Gesture):
     def __init__(self, gesture: StaticGesture, index: list[int], *others: list[dict['gesture': StaticGesture, 'offset': list[int], 'index': list[int]]]):
@@ -52,6 +55,7 @@ class MotionGesture(Gesture):
         ]
         self.hands: list[list[int | list[int]]] = []
         # Hand, Level, level start time, [x, y]
+
     def detect(self, hands: list[list[list[int]]]) -> bool:
         for num, hand in enumerate(hands):
             if hand not in self.hands or self.hands[hand][1] == -1:
@@ -76,7 +80,6 @@ class MotionGesture(Gesture):
                     self.hands[num][1] += 1
                     self.hands[num][2] = hand[index[3][0]][index[3][1]]
                     self.hands[num][3] = time.time()
-
 
 
 gestures = {
